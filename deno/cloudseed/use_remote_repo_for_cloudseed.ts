1f59acaca361aebed418a922e7e5e972f4c2fb05
automation <ci@oi.io>
2022-03-21 16:24:49 -0700

    Example and tools to switch between local and remote

--- deno/cloudseed/use_remote_repo_for_cloudseed.ts
+++ deno/cloudseed/use_remote_repo_for_cloudseed.ts
@@ -1,19 +1,17 @@
 /**
- * Use a local copy of this repo for development
- * 1. Check out the repo https://github.com/metapages/cloudseed-deno at: `./cloudseed/git/cloudseed-deno`
- * 2. Change the env var `DENO****??` in `.env` files to point to the above repo, so that [just](https://github.com/casey/just) files will consume the updated reference, and point all deno scripts to the local git repo.
- * 3. Edit and update
- * 4. After: make a PR with the new changes
+ * Use a remote copy of this repo for development
+ * 1. Remove the env var `DENO_SOURCE` in the root `.env` file.
+ *    This will make all deno scripts use the remote repo.
  */
 
 import { join } from "https://deno.land/std@0.130.0/path/mod.ts";
 import { getGitRepositoryRoot } from "../git/mod.ts";
-import { ensureLineExistsInFile } from "../fs/mod.ts";
+import { removeLineInFile } from "../fs/mod.ts";
 
 // Make sure the git repo is cloned to the local folder
 const gitRoot = await getGitRepositoryRoot();
 
-await ensureLineExistsInFile({
+await removeLineInFile({
   file: join(gitRoot, ".env"),
-  line: `DENO_SOURCE=${cloudseedGitDir}`,
+  regex: new RegExp("^DENO_SOURCE=.*"),
 });
